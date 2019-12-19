fb79cb5d563d2fcdf0842f8105e9b1a7c363b434
unknown <yhlee89.dho@gmail.com>
2019-12-18 22:04:09 -0600

    working portfolio, needs css work

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -41,47 +41,47 @@ function removeActiveList () {
 
 // Portfolio Selection
 
-let projects = [
-    mooviesGroovies= {
-        title: "Movies & Groovies",
-        discription: "Nearby movie query searh of movieglu API followed by google map routing." ,
-        imgSource: "Assets/Images/moviesAndGroovies.png" ,
-        imgAlt: "Moovies and Groovies",
-        gitPage: "https://yhlee-dho.github.io/Movies-And-Groovies/"
-    },
+// let projects = [
+//     mooviesGroovies= {
+//         title: "Movies & Groovies",
+//         discription: "Nearby movie query searh of movieglu API followed by google map routing." ,
+//         imgSource: "Assets/Images/moviesAndGroovies.png" ,
+//         imgAlt: "Moovies and Groovies",
+//         gitPage: "https://yhlee-dho.github.io/Movies-And-Groovies/"
+//     },
 
-    weatherDashboard= {
-        title: "Weather Dashboard",
-        discription: "Weather condition display with user input of target location." ,
-        imgSource: "Assets/Images/weatherDashboard.png" ,
-        imgAlt: "Weather Dashboard",
-        gitPage: "https://yhlee-dho.github.io/Server-Side-APIs-Weather-Dashboard/"
-    },
+//     weatherDashboard= {
+//         title: "Weather Dashboard",
+//         discription: "Weather condition display with user input of target location." ,
+//         imgSource: "Assets/Images/weatherDashboard.png" ,
+//         imgAlt: "Weather Dashboard",
+//         gitPage: "https://yhlee-dho.github.io/Server-Side-APIs-Weather-Dashboard/"
+//     },
 
-    workDayPlanner= {
-        title: "Work Day Planner",
-        discription: "Hourly scheduling per work-day with save and delete function for tasks." ,
-        imgSource: "Assets/Images/workDayPlanner.png" ,
-        imgAlt: "Work Day Planner",
-        gitPage: "https://yhlee-dho.github.io/Third-Party-APIs-Day-Planner/"
-    },
+//     workDayPlanner= {
+//         title: "Work Day Planner",
+//         discription: "Hourly scheduling per work-day with save and delete function for tasks." ,
+//         imgSource: "Assets/Images/workDayPlanner.png" ,
+//         imgAlt: "Work Day Planner",
+//         gitPage: "https://yhlee-dho.github.io/Third-Party-APIs-Day-Planner/"
+//     },
 
-    codeQuiz= {
-        title: "Code Quiz",
-        discription: "Basic quiz app with timer." ,
-        imgSource: "Assets/Images/codeQuiz.png" ,
-        imgAlt: "Moovies and Groovies",
-        gitPage: "https://yhlee-dho.github.io/Web-APIs-Code-Quiz/"
-    },
+//     codeQuiz= {
+//         title: "Code Quiz",
+//         discription: "Basic quiz app with timer." ,
+//         imgSource: "Assets/Images/codeQuiz.png" ,
+//         imgAlt: "Moovies and Groovies",
+//         gitPage: "https://yhlee-dho.github.io/Web-APIs-Code-Quiz/"
+//     },
 
-    passwordGenerator= {
-        title: "Password Generator",
-        discription: "Basic random string generator." ,
-        imgSource: "Assets/Images/passwordGenerator.png" ,
-        imgAlt: "Password Generator",
-        gitPage: "https://yhlee-dho.github.io/Password-Generator/"
-    },
-];
+//     passwordGenerator= {
+//         title: "Password Generator",
+//         discription: "Basic random string generator." ,
+//         imgSource: "Assets/Images/passwordGenerator.png" ,
+//         imgAlt: "Password Generator",
+//         gitPage: "https://yhlee-dho.github.io/Password-Generator/"
+//     },
+// ];
 
 
 function activeMoviesGroovies () {
@@ -89,67 +89,143 @@ function activeMoviesGroovies () {
     var element = document.getElementById("moviesGroovies");
     element.classList.add("active");
 
+    if (document.getElementById('moviesGrooviesPortfolio')) {
+
+        if (document.getElementById('moviesGrooviesPortfolio').style.display == 'none') {
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'block';
+            document.getElementById('weatherDashboardPortfolio').style.display = 'none';
+            document.getElementById('workDayPlannerPortfolio').style.display = 'none';
+            document.getElementById('codeQuizPortfolio').style.display = 'none';
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'none';
+
+        }
+        else {
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'none';
+            // document.getElementById('weatherDashboardPortfolio').style.display = 'block';
+        }
+    }
+    // rmHideMooviesGroovies();
+    // $("#moviesGooviesPortfolio").toggleClass('hide');
+
+    // let portEl = $("<div>").attr("class", "card mb-3").attr("style", "width: 100%;");
+
+    // let divEl = $("<div").attr("class", "row no-gutters");
+    // let cardEl = $("<div>").attr("class", "col-md-4");
+    // let cardImageEl = $("<img>").attr("src", `${projects.mooviesGroovies.imgSource}`).attr("class", "card-img").attr("alt", `${projects.mooviesGroovies.imgAlt}`);
+
+    // let bodyDivEl = $("<div").attr("class", "col-md-8");
+    // let bodyCardEl = $("<div>").attr("class", "card-body");
+    // let cardTitleEl = $("<h5>").attr("class", "card-title");
+    // let cardTextEl = $("<p>").attr("class", "card-text");
+
+    // portEl.append(divEl);
+
+    // divEl.append(cardEl + bodyDivEl);
     
-    let portEl = $("<div>").attr("class", "card mb-3").attr("style", "width: 100%;");
-    let divEl = $("<div").attr("class", "row no-gutters");
-    let cardEl = $("<div>").attr("class", "col-md-4");
-    let cardImageEl = $("<img>").attr("src", `${projects.mooviesGroovies.imgSource}`).attr("class", "card-img").attr("alt", `${projects.mooviesGroovies.imgAlt}`);
+    // cardEl.append(cardImageEl);
 
-    let bodyDivEl = $("<div").attr("class", "col-md-8");
-    let bodyCardEl = $("<div>").attr("class", "card-body");
-    let cardTitleEl = $("<h5>").attr("class", "card-title");
-    let cardTextEl = $("<p>").attr("class", "card-text");
-
-    portEl.append(divEl);
-    divEl.append(cardBodyEl).append(cardImageEl);
-    $("portfolioDisplay").append(portEl);
-
-
-
-// Create Card for Portfolio
-{/* <div class="card mb-3" style="max-width: 540px;">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="..." class="card-img" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div> */}
+    // bodyDivEl.append()
+    // .append(cardTitleEl + cardTextEl);
+    // $("#portfolioDisplay").append(portEl);
 
 }
+
+// function rmHideMooviesGroovies() {
+//     var rmHide = $(".hide")
+//     // document.getElementById("moviesGrooviesPortfolio");
+//     rmHide.removeClass("hide");
+// };
 
 function activeWeatherDashboard () {
     removeActivePortfolio();
     var element = document.getElementById("weatherDashboard");
     element.classList.add("active");
+
+    if (document.getElementById('weatherDashboardPortfolio')) {
+
+        if (document.getElementById('weatherDashboardPortfolio').style.display == 'none') {
+            document.getElementById('weatherDashboardPortfolio').style.display = 'block';
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'none';
+            document.getElementById('workDayPlannerPortfolio').style.display = 'none';
+            document.getElementById('codeQuizPortfolio').style.display = 'none';
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'none';
+
+        }
+        else {
+            document.getElementById('weatherDashboardPortfolio').style.display = 'none';
+            // document.getElementById('workDayPlannerPortfolio').style.display = 'block';
+        }
+    }
 }
 
 function activeWorkDayPlanner () {
     removeActivePortfolio();
     var element = document.getElementById("workDayPlanner");
     element.classList.add("active");
+
+    if (document.getElementById('workDayPlannerPortfolio')) {
+
+        if (document.getElementById('workDayPlannerPortfolio').style.display == 'none') {
+            document.getElementById('workDayPlannerPortfolio').style.display = 'block';
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'none';
+            document.getElementById('weatherDashboardPortfolio').style.display = 'none';
+            document.getElementById('codeQuizPortfolio').style.display = 'none';
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'none';
+
+        }
+        else {
+            document.getElementById('workDayPlannerPortfolio').style.display = 'none';
+            // document.getElementById('codeQuizPortfolio').style.display = 'block';
+        }
+    }
 }
 
 function activeCodeQuiz () {
     removeActivePortfolio();
     var element = document.getElementById("codeQuiz");
     element.classList.add("active");
+
+    if (document.getElementById('codeQuizPortfolio')) {
+
+        if (document.getElementById('codeQuizPortfolio').style.display == 'none') {
+            document.getElementById('codeQuizPortfolio').style.display = 'block';
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'none';
+            document.getElementById('weatherDashboardPortfolio').style.display = 'none';
+            document.getElementById('workDayPlannerPortfolio').style.display = 'none';
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'none';
+
+        }
+        else {
+            document.getElementById('codeQuizPortfolio').style.display = 'none';
+            // document.getElementById('passwordGeneratorPortfolio').style.display = 'block';
+        }
+    }
 }
 
 function activePasswordGenerator () {
     removeActivePortfolio();
     var element = document.getElementById("passwordGenerator");
     element.classList.add("active");
+
+    if (document.getElementById('passwordGeneratorPortfolio')) {
+
+        if (document.getElementById('passwordGeneratorPortfolio').style.display == 'none') {
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'block';
+            document.getElementById('moviesGrooviesPortfolio').style.display = 'none';
+            document.getElementById('weatherDashboardPortfolio').style.display = 'none';
+            document.getElementById('workDayPlannerPortfolio').style.display = 'none';
+            document.getElementById('codeQuizPortfolio').style.display = 'none';
+
+        }
+        else {
+            document.getElementById('passwordGeneratorPortfolio').style.display = 'none';
+            // document.getElementById('moviesGrooviesPortfolio').style.display = 'block';
+        }
+    }
 }
 
 function removeActivePortfolio () {
-    $("#portfolioDisplay").empty();
+    // $("#moviesGrooviesPortfolio").empty();
     var rmMoviesGroovies = document.getElementById("moviesGroovies");
     rmMoviesGroovies.classList.remove("active");
     var rmWeatherDashboard = document.getElementById("weatherDashboard");
@@ -163,4 +239,3 @@ function removeActivePortfolio () {
 }
 
 
-// let projects;
